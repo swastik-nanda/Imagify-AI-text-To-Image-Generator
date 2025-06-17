f826ec4c674b599da8a598ed140da1cc4918d532
Swastik Nanda <swastik2004nanda@gmail.com>
2025-06-17 17:31:35 +0530

    changins cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ai-text-to-image-frontend.onrender.com/",
+  })
+);
 
 // Use top-level await here
 await connectDB();
